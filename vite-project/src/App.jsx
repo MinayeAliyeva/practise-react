0cae461d700af82dcc98410482b9e8979cdfa86d
MinayeAliyeva <7s346pu@code.edu.az>
2023-11-14 02:07:50 +0400

    added react-bootstrap

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,33 +3,13 @@ import { useReducer, useState } from "react";
 import "./App.css";
 import Name from "./Components/Name";
 import CardCom from "./Components/CardCom";
-
+import TodoApp from "./todo";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  // const initialValue = 0;
-  // const reducer = (state, action) => {
-  //   switch (action) {
-  //     case "increment":
-  //       return state + 1;
-  //     case "dec":
-  //       return state - 1;
-  //     default:
-  //       return state;
-  //   }
-  // };
-  // const [state, dispatch] = useReducer(reducer, initialValue);
-  let skills = ["html", "css", "js"];
-  let userName = "Jhon Doe";
-  const sayHi = () => {
-    console.log(`Hi ${userName}`);
-  };
+  const [fullName, setFullName] = useState("Jhon Doe");
   return (
     <>
-      {/* <button onClick={() => dispatch("increment")}>+</button>
-      <span>{state}</span>
-      <button onClick={() => dispatch("dec")}>-</button> */}
-      <h1>{userName}</h1>
-      <Name userName={userName} />
-      <CardCom func={sayHi} arr={skills} />
+      <TodoApp />
     </>
   );
 }
